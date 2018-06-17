da4c1ecd6c6d073bcb3daa3e92032ff7b184911b
Sridip <38683560+Sensrdt@users.noreply.github.com>
2018-06-16 22:50:39 -0700

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+   {
+   	id: 1,
+   	name: 'Sridip ',
+   	username:'sensrdt',
+   	email:'stsdutta2@gmail.com'
+   },
+   {
+   	id: 2,
+   	name: 'Vidhyansu ',
+   	username:'vidhujain',
+   	email:'vidhujn@gmail.com'
+   },
+   {
+   	id: 3,
+   	name: 'Lavender ',
+   	username:'Lavendroers',
+   	email:'lavender@gmail.com'
+   },
+   {
+   	id: 4,
+   	name: 'Casmero ',
+   	username:'casmerios',
+   	email:'Casmero@gmail.com'
+   },
+   {
+   	id: 5,
+   	name: 'John ',
+   	username:'jhonathan',
+   	email:'John@gmail.com'
+   },
+   {
+   	id: 6,
+   	name: 'Peter ',
+   	username:'petersens',
+   	email:'Peter@gmail.com'
+   },
+   {
+   	id: 7,
+   	name: 'Rapeen ',
+   	username:'Rappenns',
+   	email:'Rapeen@gmail.com'
+   },
+   {
+   	id: 8,
+   	name: 'Estas ',
+   	username:'Evendro',
+   	email:'Estas@gmail.com'
+   },
+
+   {
+   	id: 9,
+   	name: 'Comos ',
+   	username:'Comoestas',
+   	email:'Comos@gmail.com'
+   },
+   {
+   	id: 10,
+   	name: 'Lmao ',
+   	username:'Lmaos',
+   	email:'Lmao@gmail.com'
+   }
+
+]
